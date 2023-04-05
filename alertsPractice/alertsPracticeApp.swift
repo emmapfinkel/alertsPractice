7fc29fbb0daf098ee1d7f7604d79c58289239036
Emma Finkel <emma.finkel@gmail.com>
2023-04-05 10:59:15 -0400

    Initial Commit

---
+++ alertsPractice/alertsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  alertsPracticeApp.swift
+//  alertsPractice
+//
+//  Created by scholar on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct alertsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
